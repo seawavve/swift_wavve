f0ec5f1a6d3a0bd2bb5a68eaa544801bf566353d
seawavve <66352658+seawavve@users.noreply.github.com>
2021-01-22 11:43:38 +0900

    Update tutorial.swift

--- tutorial.swift
+++ tutorial.swift
@@ -6,3 +6,47 @@ var tmpVar=10
 tmpvar=20
 let tmpConst=30
 
+
+//Integer+String
+let apples = 3
+print("I have \(apples) apples.")
+
+//List
+var shoppingList=["apple","orange"]
+//Dict
+var deathpoll=[
+"Hani":"3"
+]
+
+//Empty List
+var emptyArray=[String]()
+var emptyDict=[String:Float]()
+var emptyArray=[]
+var emptyDict=[:]
+
+//if
+let item = 30
+if item>20 {print("over 20")}
+
+//for
+let i = [1,2,3]
+for j in i{
+    print("Chapter \(j)")
+}
+
+//func
+func greet(name: String, day: String) -> String {
+    return "Hello \(name), today is \(day)."
+}
+print(greet(name: "Hani", day: "Friday"))
+
+//class
+class Shape {
+    var numberOfSides = 0
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
+var shape = Shape()
+shape.numberOfSides = 7
+var shapeDescription = shape.simpleDescription()
